affabbdd6f062f69f2e23f3a8f71723dca6945cb
yessine <yessine.agrebi.aiesec@gmail.com>
2023-10-14 20:18:40 +0200

    fetch programs with swr

--- app/programs/[programId]/page.tsx
+++ app/programs/[programId]/page.tsx
@@ -1,14 +1,18 @@
 "use client";
 import API from "@/api/API";
 import { AccordionCustomIcon } from "@/components/ui/AccordionCustomIcon";
-import { ProgramData } from "@/lib/types";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Exercise, ProgramData, Workout } from "@/lib/types";
 import { useEffect, useState } from "react";
 
 export default function Page({ params }: { params: { programId: number } }) {
-  const [program, setProgram] = useState<ProgramData[]>([]);
+  //convert to type object
+
+  const [program, setProgram] = useState<ProgramData>();
 
   useEffect(() => {
-    API.get<ProgramData[]>(`/programs/workouts/${params.programId}`)
+    API.get<ProgramData>(`/programs/workouts/${params.programId}`)
       .then((res) => {
         console.log(res.data);
         setProgram(res.data);
@@ -20,23 +24,25 @@ export default function Page({ params }: { params: { programId: number } }) {
 
   return (
     <div className="w-full h-full flex items-center justify-center my-5">
-      {program?.map((program) => (
-        <div className="border mx-2 p-4 md:w-3/4 w-full h-full flex flex-col items-center justify-center rounded-lg shadow-md">
-          <h1 className="text-2xl font-bold mb-2">
-            {program.program.programName}
-          </h1>
-          <p className="mb-4">{program.program.description}</p>
-          <div className="flex flex-col gap-10 w-full">
-            {program.exercises.map((workout, index) => (
-              <AccordionCustomIcon
-                key={index}
-                open={index + 1}
-                workout={workout}
-              />
-            ))}
-          </div>
+      <div className="border mx-2 p-4 md:w-3/4 w-full h-full flex flex-col items-center justify-center rounded-lg shadow-md">
+        <h1 className="text-2xl font-bold mb-2">{program?.programName}</h1>
+        <p className="mb-4">{program?.description}</p>
+        <div className="flex flex-col gap-10 w-full">
+          {program?.workouts.map((workout, index) => {
+            return (
+              <Card key={index}>
+                <CardHeader>
+                  <CardTitle>{workout.workoutName}</CardTitle>
+                  <CardDescription>{new Date(program.endDate).toISOString().split("T")[0]}</CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <Button variant="outline" color="blue" size="sm">Open</Button>
+                </CardContent>
+              </Card>
+            );
+          })}
         </div>
-      ))}
+      </div>
     </div>
   );
 }
