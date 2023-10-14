b90b8c7311a2b3fec7a281ab3c7a0bb9eebadf92
yessine <yessine.agrebi.aiesec@gmail.com>
2023-10-14 20:55:39 +0200

    fetch workouts data and display cards

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,13 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import API from "@/api/API";
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const fetcher = (url: string) =>
+  API.get(url).then((res) => {
+    console.log(res.data);
+    return res.data;
+  });
